965244be83b34511cecc19b193e73d90e9251f91
ZèD <imzihad@gmail.com>
2021-06-02 17:35:00 +0000

    sakura: Set Required Device VARs
    Signed-off-by: YadavMohit19 <mohityadav050575@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,91 @@
 #
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021 The OrangeFox Recovery Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
 #
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="sakura"
+#set -o xtrace
 
-add_lunch_combo omni_sakura-userdebug
-add_lunch_combo omni_sakura-eng
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+   	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+   	export TW_DEFAULT_LANGUAGE="en"
+	export LC_ALL="C"
+ 	export ALLOW_MISSING_DEPENDENCIES=true
+   	export TARGET_DEVICE_ALT="sakura_india"
+	export OF_TARGET_DEVICES="sakura,sakura_india"
+	export FOX_RESET_SETTINGS="disabled"
+   	export FOX_USE_BASH_SHELL=1
+   	export FOX_ASH_IS_BASH=1
+   	export FOX_USE_NANO_EDITOR=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_SED_BINARY=1
+    export FOX_USE_XZ_UTILS=1
+   	export FOX_REPLACE_BUSYBOX_PS=1
+   	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+   	export OF_USE_MAGISKBOOT="1"
+   	export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+   	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+	export OF_USE_SYSTEM_FINGERPRINT="1"
+
+	# MIUI
+	export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+
+	# dm-verity/forced-encryption
+	export OF_KEEP_DM_VERITY=1
+   	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+
+        # -- add settings for R11 --
+        export FOX_R11=1
+        export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
+        # -- end R11 settings --
+
+	# run a process after formatting data to work-around MTP issues
+	export OF_RUN_POST_FORMAT_PROCESS=1
+
+	# screen
+        export OF_SCREEN_H=2280
+        export OF_STATUS_H=95
+        export OF_STATUS_INDENT_LEFT=50
+        export OF_STATUS_INDENT_RIGHT=50
+
+	# let's log what are the build VARs that we used
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "PLATFORM_" >> $FOX_BUILD_LOG_FILE
+  	fi
+
+	add_lunch_combo omni_"$FDEVICE"-eng
+fi
+
